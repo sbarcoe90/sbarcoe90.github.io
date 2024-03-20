cc51efc7b7b1191b1de1cd640c82804382df7813
sbarcoe90 <sbarcoe@outlook.ie>
2024-03-20 15:57:48 +0000

    setting up hamburger for mobile

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-const sidebar = document.querySelector('.sidebar');
-const content = document.querySelector('.content');
-
-sidebar.addEventListener('mouseenter', expandMenu);
-sidebar.addEventListener('mouseleave', collapseMenu);
-
-function expandMenu() {
-    sidebar.style.width = '30%'; // Adjust this value to match the desired width
-    content.style.marginLeft = '30%'; // Adjust this value to match the desired width
-}
-
-function collapseMenu() {
-    sidebar.style.width = '15%'; // Adjust this value to match the desired collapsed width
-    content.style.marginLeft = '15%'; // Adjust this value to match the desired collapsed width
+function toggleMenu() {
+    var menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
 }
