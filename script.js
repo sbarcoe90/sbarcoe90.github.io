c6d63d7e18d0cb4554f88c1d3e5ad69dd716970c
sbarcoe90 <sbarcoe@outlook.ie>
2024-03-20 15:55:00 +0000

    adjusted stlyes to % in scipty

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ sidebar.addEventListener('mouseenter', expandMenu);
 sidebar.addEventListener('mouseleave', collapseMenu);
 
 function expandMenu() {
-    sidebar.style.width = '250px'; // Adjust this value to match the desired width
-    content.style.marginLeft = '250px'; // Adjust this value to match the desired width
+    sidebar.style.width = '30%'; // Adjust this value to match the desired width
+    content.style.marginLeft = '30%'; // Adjust this value to match the desired width
 }
 
 function collapseMenu() {
-    sidebar.style.width = '50px'; // Adjust this value to match the desired collapsed width
-    content.style.marginLeft = '50px'; // Adjust this value to match the desired collapsed width
+    sidebar.style.width = '15%'; // Adjust this value to match the desired collapsed width
+    content.style.marginLeft = '15%'; // Adjust this value to match the desired collapsed width
 }
